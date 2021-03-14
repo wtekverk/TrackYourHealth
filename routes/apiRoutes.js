35e7f6bbbcf36ecd676c418a19cc3e05d1d0ae95
Wendy Tekverk <wtekverk@gmail.com>
2021-03-14 15:49:46 -0600

    psudo coding out apiRoutes page

---
+++ routes/apiRoutes.js
@@ -0,0 +1,40 @@
+const Workout = require("../models /workout")
+
+module.exports = function (app) {
+
+
+
+    //get api/workouts
+
+
+
+
+
+    //post api/workouts
+
+
+
+    //get api/workouts/range
+
+
+
+    //post api/workouts/range
+
+
+
+
+    //put api/workouts/:id 
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
