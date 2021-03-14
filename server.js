18a9caa4c4ec3202a583aff3f92b52a9cc4d872a
Wendy Tekverk <wtekverk@gmail.com>
2021-03-14 14:53:58 -0600

    added server information NOTE TO SELF: look at activety 17 for info on server if trouble arrises

---
+++ server.js
@@ -0,0 +1,36 @@
+//Dependencies 
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+
+//setting port to listen on 
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(logger("dev"));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrack", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+
+
+//Attaching routes 
+require('./routes/api-routes')(app)
+require('./routes/html-routes')(app)
+
+
+
+//console log of port running 
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}..`);
+})
